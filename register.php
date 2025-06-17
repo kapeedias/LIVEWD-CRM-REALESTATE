526228823b4d9b5762164890428b3dcb7d32cbfd
Sai Deepak <sai@livewd.ca>
2025-06-17 14:04:03 -0700

    test

--- register.php
+++ register.php
@@ -164,7 +164,7 @@ $csrfToken = generateCsrfToken();
                                 <hr />
                         <?php endif; ?>
                      <form class="forms-register" id="forms-register" method="POST" action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>">
-                      <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($csrf_token) ?>">
+                      <input type="hidden" name="csrf_token" value="<?= $csrf_token; ?>">
                       <div class="form-group">
                         <label for="firstName">First Name</label>
                         <input type="text" class="form-control" id="firstName" name="first_name" required>
