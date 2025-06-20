263e37ab6a1989c54f6bc26b611315bc63d9ee5c
Sai Deepak <sai@livewd.ca>
2025-06-19 20:36:31 -0700

    test

--- register.php
+++ register.php
@@ -3,7 +3,9 @@ session_start();
 require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/config/db.php';
 require_once __DIR__ . '/classes/users.php';
-$user = new User($pdo); // Use the PDO object created in db.php
+
+$user = new User($pdo); 
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $formData = [
