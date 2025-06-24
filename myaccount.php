5e598b2f44e122d9c84f7058a026c53daa0f2602
Sai Deepak <sai@livewd.ca>
2025-06-24 10:45:39 -0700

    myacount

--- myaccount.php
+++ myaccount.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -615,7 +620,15 @@
 
         <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
           <div>
-            <h4 class="mb-3 mb-md-0">Welcome to Dashboard</h4>
+            <h4 class="mb-3 mb-md-0">
+            <?php
+            echo $_SESSION['user_id']."<br />";
+            echo $_SESSION['user_name']."<br />";
+            echo $_SESSION['user_email']."<br />";
+            ?>
+
+
+            </h4>
           </div>
           <div class="d-flex align-items-center flex-wrap text-nowrap">
             <div class="input-group date datepicker dashboard-date mr-2 mb-2 mb-md-0 d-md-none d-xl-flex" id="dashboardDate">
