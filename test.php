5e5b646879f2ba30126ec9c79f2f6f1c3e93caed
Sai Deepak <sai@livewd.ca>
2025-06-24 00:36:35 -0700

    test

--- test.php
+++ test.php
@@ -1,35 +1,12 @@
 <?php
-session_start();
-
-require_once __DIR__ . '/config/config.php';
-require_once __DIR__ . '/config/db.php';
-
-try {
-    $pdo = Database::getInstance();
-} catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
-}
-
-$userId = 46;
-$message = '';
 
 
-    // Fetch hashed password from DB
-    $stmt = $pdo->prepare("SELECT pwd FROM general_info_users WHERE id = :id LIMIT 1");
-    $stmt->execute(['id' => $userId]);
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if (!$user) {
-        echo $message = "User with ID $userId not found.";
-    } else {
-        $hashedPassword = $user['pwd'];
-
-        if (password_verify('@_aPTE=I_Qx)2m21Ojstl', $hashedPassword)) {
+        if (password_verify('>=p&1~F~[*um18PB8*xJG', '$2y$10$ZIu6qWpsst0KYDCgFFkeluAqlWOLaBz7/y6IH7fdfrCyo4FPytD9C')) {
              echo $message = "✅ Password is CORRECT!";
         } else {
             echo $message = "❌ Password is INCORRECT.";
         }
-    }
+   
 
 
     
