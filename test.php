91d79f9e403c1610b9ee020f1f09c1d3f11b1f1a
Sai Deepak <sai@livewd.ca>
2025-06-24 23:50:49 -0700

    test

--- test.php
+++ test.php
@@ -4,8 +4,12 @@ require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/classes/Mailer.php';
 
 $mailer = new Mailer();
-$sent = $mailer->sendResetPasswordEmail('deeps450@gmail.com', 'Sai Deepak', '0987654321');
+$sent = $mailer->sendResetPasswordEmail('info@livewd.ca', 'John Doe', '1234565');
 
-echo $sent ? "Email sent!" : "Failed to send.";
-    
+if ($sent) {
+    echo "Reset link sent!";
+} else {
+    echo "Failed to send reset link.";
+}
+  
 ?>
